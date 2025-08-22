6481ec497d6a35487cc4d1665d592ad72beff132
Tink <85289337+tink012184@users.noreply.github.com>
2025-08-21 20:13:17 -0500

    fixed name of burgerstand file and added quote file

---
+++ week_2/quote.py
@@ -0,0 +1,10 @@
+"""
+Author: Melissa Lutz
+Date: August 21, 2025
+File Name: quote.py
+Description: This program prints a favorite quote with credit to the author.
+"""
+
+quote = "“Happiness can be found even in the darkest of times, if one only remembers to turn on the light.” – J.K. Rowling"
+
+print(quote)
